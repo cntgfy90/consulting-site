193391043eee80c08be99c8e10c71a373164bfaf
cntgfy90 <stepankolomiyets@gmail.com>
2018-11-10 16:42:55 +0200

    Set Home Page start

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7dc8ba4e5cbe66f334f5";
+/******/ 	var hotCurrentHash = "15a2833bbb206ac46527";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
